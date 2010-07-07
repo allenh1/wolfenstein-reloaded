472fbacde627398a6dbe9f45e52ae9f4824078db
hunterallen40 <hunterallen40@14159fd0-a646-01e2-d422-a234b5f98ae5>
2010-07-07 17:57:43 +0000

    Modified eWall stuff (just barely scratched the surface...)

--- trunk/wolf/src/game/engine/eWall.h
+++ trunk/wolf/src/game/engine/eWall.h
@@ -1,6 +1,48 @@
-#ifndef EWALL_H_INCLUDED
-#define EWALL_H_INCLUDED
+/*
 
+ **************************************************************************
 
+ Wolfenstein Reloaded
+ Developed by Morgan Jones <maclover201@me.com>
+ File Description: Prototype for all other files
 
-#endif // EWALL_H_INCLUDED
+ **************************************************************************
+
+ Copyright © 2010 Morgan Jones
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ **************************************************************************
+
+ */
+
+#include "defs.h"
+#include "rWall.h"
+#include "rObject.h"
+
+#ifndef WOLF_EWALL_H
+#define WOLF_EWALL_H
+
+/*!
+ *	This class is a test.
+ *  Detailed description is in this block.
+ */
+
+class eWall: public rWall, private rPoint {
+	public:
+        void collisionDetection(rVertex( );
+
+};
+
+#endif
